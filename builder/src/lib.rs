a1866042d1267755a887b781516ba6878f6ca1ae
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-11-29 20:25:51 +0100

    feature flags

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,12 @@
+#[cfg(feature = "markdown")]
 pub mod markdown;
 pub mod pickers;
 mod pipeline;
+#[cfg(feature = "pretty_url")]
 pub mod pretty_url;
+#[cfg(feature = "tailwind")]
 pub mod tailwind;
+#[cfg(feature = "template")]
 pub mod template;
 pub mod traits;
 
